f5c14148a0347aab77e1a379f88ba77f33db394f
dev-redo <mis05041@naver.com>
2023-04-03 02:57:14 +0900

    chore: setting storybook in shared-ui package

--- packages/shared-ui/vite.config.ts
+++ packages/shared-ui/vite.config.ts
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import dts from 'vite-plugin-dts';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
-  build: {
-    lib: {
-      entry: './src/index.ts',
-      formats: ['cjs', 'es'], // Specify the formats that the library should be built in
-      fileName: format => `index.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'], // Specify the external dependencies that the library relies on
-    },
-  },
-});
+  plugins: [react()],
+})
