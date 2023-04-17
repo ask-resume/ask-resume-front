a1994b433ccefd6ac9e8b0ec05a71ae099969c39
dev-redo <mis05041@naver.com>
2023-04-18 08:31:14 +0900

    feat: render spinner when fetching job data

--- app/src/pages/[locale]/form/user-info.tsx
+++ app/src/pages/[locale]/form/user-info.tsx
@@ -13,6 +13,7 @@ import InputSelect, { useInputSelectedState } from 'shared-ui/src/components/Inp
 import Select, { Option, useSelectState } from 'shared-ui/src/components/Select';
 import Slider, { useSliderState } from 'shared-ui/src/components/Slider';
 import Icon from 'shared-ui/src/components/Icon';
+import Spinner from 'shared-ui/src/components/Spinner';
 
 import { getI18nProps, getStaticPaths } from '../../../i18n/getStatic';
 import { useJobs } from '@/api/form';
@@ -55,9 +56,7 @@ export default function UserInfo() {
     setSelectedYearsOfCareer(0);
   }, [locale]);
 
-  // TODO: isLoading일 때 Option List에 loading바를 렌더링하게끔 수정
-  const { data: jobs, isLoading } = useJobs(locale);
-  if (isLoading) return <div>Loading</div>;
+  const { data: jobs, isLoading: isJobsLoading } = useJobs(locale);
 
   return (
     <>
@@ -68,84 +67,94 @@ export default function UserInfo() {
       </Head>
 
       <main className={styles._CONTAINER_}>
-        {/* Job InputSelect */}
-        <InputSelect
-          className={styles._SELECT_}
-          selectedOption={selectedJob}
-          onChangeSelectedOption={onChangeSelectedJob}
-          options={jobs!}
-          labelText={t('label.job') ?? ''}
-          placeholder={t('placeholder.job') ?? ''}
-          labelWeight={LABEL_WEIGHT}
-          labelSize={LABEL_SIZE}
-        />
-
-        {/* Nation InputSelect */}
-        <Select
-          className={styles._SELECT_}
-          selectedOption={selectedLanguage}
-          onChangeSelectedOption={onChangeSelectedLanguage}
-          options={NATION_OPTION}
-          labelText={t('label.nation') ?? ''}
-          placeholder={t('placeholder.nation') ?? ''}
-          labelWeight={LABEL_WEIGHT}
-          labelSize={LABEL_SIZE}
-        />
-
-        {/* Interview Difficulty InputSelect */}
-        <Select
-          className={styles._SELECT_}
-          selectedOption={selectedDifficulty}
-          onChangeSelectedOption={onChangeSelectedDifficulty}
-          options={DIFFICULTY_OPTION}
-          labelText={t('label.difficulty') ?? ''}
-          placeholder={t('placeholder.difficulty') ?? ''}
-          labelWeight={LABEL_WEIGHT}
-          labelSize={LABEL_SIZE}
-        />
-
-        {/* Years of Experience Slider */}
-        <div>
-          <div style={{ display: 'flex', gap: '1rem', alignItems: 'center' }}>
-            <Text className="_label" size={LABEL_SIZE} weight={LABEL_WEIGHT}>
-              {t('label.years-of-experience')}
-            </Text>
-            <Divider variant="vertical" color={ColorMap.gray_5} width={2} />
-            <Text variant="block" size="small" weight="medium" textColor={ColorMap.blue_5}>
-              {formatYearsOfCareer({ selectedYearsOfCareer, locale, t })}
-            </Text>
+        {isJobsLoading && (
+          <div>
+            <Spinner size="xl" />
           </div>
+        )}
 
-          <Slider
-            size="medium"
-            min={0}
-            max={10}
-            step={1}
-            inputValue={selectedYearsOfCareer}
-            onChangeInputValue={setSelectedYearsOfCareer}
-          />
-        </div>
+        {!isJobsLoading && (
+          <>
+            {/* Job InputSelect */}
+            <InputSelect
+              className={styles._SELECT_}
+              selectedOption={selectedJob}
+              onChangeSelectedOption={onChangeSelectedJob}
+              options={jobs!}
+              labelText={t('label.job') ?? ''}
+              placeholder={t('placeholder.job') ?? ''}
+              labelWeight={LABEL_WEIGHT}
+              labelSize={LABEL_SIZE}
+            />
 
-        {/* Routing Button of User Resume  */}
-        <Link href="/form/user-resume">
-          <Button
-            size="lg"
-            buttonColor="blue"
-            fullWidth
-            disabled={
-              !validateUserInfoForm({
-                selectedJob,
-                selectedLanguage,
-                selectedDifficulty,
-                selectedYearsOfCareer,
-              })
-            }
-            label={{
-              labelText: t('label.go-to-resume-page') ?? '',
-              labelTailingIcon: <Icon.Arrow flip />,
-            }}
-          />
-        </Link>
+            {/* Nation InputSelect */}
+            <Select
+              className={styles._SELECT_}
+              selectedOption={selectedLanguage}
+              onChangeSelectedOption={onChangeSelectedLanguage}
+              options={NATION_OPTION}
+              labelText={t('label.nation') ?? ''}
+              placeholder={t('placeholder.nation') ?? ''}
+              labelWeight={LABEL_WEIGHT}
+              labelSize={LABEL_SIZE}
+            />
+
+            {/* Interview Difficulty InputSelect */}
+            <Select
+              className={styles._SELECT_}
+              selectedOption={selectedDifficulty}
+              onChangeSelectedOption={onChangeSelectedDifficulty}
+              options={DIFFICULTY_OPTION}
+              labelText={t('label.difficulty') ?? ''}
+              placeholder={t('placeholder.difficulty') ?? ''}
+              labelWeight={LABEL_WEIGHT}
+              labelSize={LABEL_SIZE}
+            />
+
+            {/* Years of Experience Slider */}
+            <div>
+              <div style={{ display: 'flex', gap: '1rem', alignItems: 'center' }}>
+                <Text className="_label" size={LABEL_SIZE} weight={LABEL_WEIGHT}>
+                  {t('label.years-of-experience')}
+                </Text>
+                <Divider variant="vertical" color={ColorMap.gray_5} width={2} />
+                <Text variant="block" size="small" weight="medium" textColor={ColorMap.blue_5}>
+                  {formatYearsOfCareer({ selectedYearsOfCareer, locale, t })}
+                </Text>
+              </div>
+
+              <Slider
+                size="medium"
+                min={0}
+                max={10}
+                step={1}
+                inputValue={selectedYearsOfCareer}
+                onChangeInputValue={setSelectedYearsOfCareer}
+              />
+            </div>
+
+            {/* Routing Button of User Resume  */}
+            <Link href="/form/user-resume">
+              <Button
+                size="lg"
+                buttonColor="blue"
+                fullWidth
+                disabled={
+                  !validateUserInfoForm({
+                    selectedJob,
+                    selectedLanguage,
+                    selectedDifficulty,
+                    selectedYearsOfCareer,
+                  })
+                }
+                label={{
+                  labelText: t('label.go-to-resume-page') ?? '',
+                  labelTailingIcon: <Icon.Arrow flip />,
+                }}
+              />
+            </Link>
+          </>
+        )}
       </main>
     </>
   );
