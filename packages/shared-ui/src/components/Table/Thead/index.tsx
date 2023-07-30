43ac1825a4737a11395539e9866331d46a44d196
m-dzn <mdzn.dev@gmail.com>
2023-07-31 06:44:13 +0900

    feat(shared-ui): add Thead align option

--- packages/shared-ui/src/components/Table/Thead/index.tsx
+++ packages/shared-ui/src/components/Table/Thead/index.tsx
@@ -1,11 +1,14 @@
 import { ReactNode } from 'react';
+import cn from 'classnames';
+import './index.scss';
 
 export interface TheadProps {
   children?: ReactNode;
+  align?: 'left' | 'center' | 'right';
 }
 
-const Thead = ({ children }: TheadProps) => {
-  return <thead>{children}</thead>;
+const Thead = ({ children, align }: TheadProps) => {
+  return <thead className={cn('_Thead_', align)}>{children}</thead>;
 };
 
 export default Thead;
