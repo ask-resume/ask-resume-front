87bd540bccccc38400e3a8f06ad59a328ec739a1
m-dzn <mdzn.dev@gmail.com>
2023-08-26 01:01:14 +0900

    feature: add Drag & Drop feature to DropZone

--- apps/web/src/modules/pdf/components/DropZone/DropZone.tsx
+++ apps/web/src/modules/pdf/components/DropZone/DropZone.tsx
@@ -8,17 +8,28 @@ interface DropZoneProps {
   accept?: string;
   hintText?: string;
   onChange?: React.ChangeEventHandler;
+  onDrop: React.DragEventHandler;
 }
 
-const DropZone = ({ accept, hintText = 'Drag & Drop a file', onChange }: DropZoneProps) => {
+const DropZone = ({ accept, hintText = 'Drag & Drop a file', onChange, onDrop }: DropZoneProps) => {
   const fileInputRef = useRef(null);
 
   const handleClick: React.MouseEventHandler = () => {
     fileInputRef.current?.click();
   };
 
+  const handleDrop: React.DragEventHandler = event => {
+    event.preventDefault();
+    onDrop(event);
+  };
+
   return (
-    <div className={styles.DropZone} onClick={handleClick}>
+    <div
+      className={styles.DropZone}
+      onClick={handleClick}
+      onDrop={handleDrop}
+      onDragOver={event => event.preventDefault()}
+    >
       <input ref={fileInputRef} type="file" hidden accept={accept} onChange={onChange} />
       <Icon.AirPlane />
       <p className={styles.hintText}>{hintText}</p>
